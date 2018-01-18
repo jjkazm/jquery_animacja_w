477adb1c3d007c9eaef248cd3ae34a2dac0b590a
Jakub Kazmierczak <kazmierczak88@gmail.com>
2018-01-18 01:09:10 +0100

    ukonczona animacja

--- js/main.js
+++ js/main.js
@@ -1,31 +1,48 @@
 'use strict';
 
-$(function(){
-    var index = 0;
-    var allSlides = $(".single-slide");
-    console.log(allSlides.length);
-    
-    console.log(allSlides[index])
-    
-    $('.single-slide').eq(index).css("width","100%");
-    
-    $('button.prev-slide').on({
-        'click': function(){
-            if(index>0){
-                index--;
-            }
-            $('.single-slide').css("width","0");
-            $('.single-slide').eq(index).css("width","100%");
-        }
-    });
-     $('button.next-slide').on({
-        'click': function(){
-            if(index<allSlides.length-1){
-                index++;
-            }
-            $('.single-slide').css("width","0");
-            $('.single-slide').eq(index).css("width","100%");
-        }
-    })
-console.log(index);    
-})
+$(function () {
+            var index = 0;
+            var allSlides = $(".single-slide");
+            $('.single-slide').eq(0).css({
+                left: '0'
+            })
+            $('.slide-caption').eq(0).css({
+                opacity: '1'
+            })
+                $('button.prev-slide').on({
+                    'click': function () {
+                        if (index > 0) {
+                            index--;
+                        }
+                        $('.single-slide').eq(index).animate({
+                            left: '0'
+                        }, 800, caption)
+                        $('.single-slide').eq(index + 1).animate({
+                            left: '100%'
+                        }, 800)
+
+                    }
+                });
+                $('button.next-slide').on({
+                    'click': function () {
+                        if (index < allSlides.length - 1) {
+                            index++;
+                        }
+                        $('.single-slide').eq(index - 1).animate({
+                            left: '-100%'
+                        }, 800, caption)
+                        $('.single-slide').eq(index).animate({
+                            left: '0'
+                        }, 800)
+                    }
+                })
+
+                function caption() {
+                    $('.slide-caption').css({
+                        opacity: '0'
+                    })
+                    $('.slide-caption').eq(index).animate({
+                        opacity: '1'
+                    }, 1500)
+                }
+            })
